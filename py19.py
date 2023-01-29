f5c886d8c480fcff1e59e55675257f66a2296bbb
Olzhas <olzhas.koshkarbay@gmail.com>
2023-01-29 14:49:14 +0600

    the first commit

--- py19.py
+++ py19.py
@@ -1,1 +1,12 @@
-print("nurzhan hello")
+
+n = int(input())
+l = []
+for i in range(n):
+    c = int(input())
+    l.append(c)
+max1 = max(l)
+min1 = min(l)
+l.remove(max1)
+l.remove(min1)
+for num in l:
+    print(num)
