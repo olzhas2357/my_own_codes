5c4008e2c724a21eff1c90c296f0aafc1549ff6c
Olzhas <olzhas.koshkarbay@gmail.com>
2023-01-29 22:00:45 +0600

    sort method

--- py17.py
+++ py17.py
@@ -1,17 +1,22 @@
-a = int(input())
-l1 = []
-l2 = []
-l3 = []
-l = []
-for i in range(a):
-    n = int(input())
-    if n < 0:
-        l1.append(n)
-    elif n == 0:
-        l2.append(n)
-    elif n > 0:
-        l3.append(n)
-l.extend(l1)
-l.extend(l2)
-l.extend(l3)
-print(l)
+
+
+w = input().split()
+for i in range(len(w)):
+    w[i] = int(w[i])
+w.sort()
+for num in w:
+    print(num, end=" ")
+print()
+w.sort(reverse = True)
+for sum in w:
+    print(sum, end =" ")
+
+# or
+
+w = input().split()
+for i in range(len(w)):
+    w[i] = int(w[i])
+w.sort()
+print(*w)
+w.sort(reverse = True)
+print(*w)
