8e412427aa297e69afca800951a7761f70bc5454
Olzhas <olzhas.koshkarbay@gmail.com>
2023-02-03 21:29:58 +0600

    funtion

--- py36.py
+++ py36.py
@@ -12,6 +12,27 @@ def is_password_good(let):
         elif n.isdigit():
             flag3 = True
     return flag1 and flag2 and flag3
-
 n = input()
 print(is_password_good(n))
+
+
+def get_next_prime(num):
+    num+=1
+    for i in range(2, num):
+        if num % i == 0:
+            return get_next_prime(num)
+    return num
+n = int(input())
+print(get_next_prime(n))
+
+
+def is_correct_bracket(s):
+    sum = 0
+    while '()' in s:
+        s = s.replace("()", "")
+    if s == "":
+        return True
+    return False
+if __name__ == "__main__":
+    w = input()
+    print(is_correct_bracket(w))
